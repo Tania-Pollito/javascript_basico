9dc8725aa1f29def6bc5fc00a257d6575013661b
Tania-Pollito <taniamorenomorales@gmail.com>
2025-06-09 16:52:50 -0500

    Arreglos, bucles y condicional

--- script.js
+++ script.js
@@ -3,3 +3,29 @@ function mostrarMensaje(){
     //alert permite visualizar un cuadro de dialogo
     alert("Hola Mundo :");
 }
+//variables
+function imprimirVariables() {
+    let nombre = "Tania";
+    let edad= 22;
+    //Forma tradicional
+    console.log("nombre: ", nombre);
+    console.log("edad: ", edad);
+    //Forma moderna de concatenar cadenas
+    console.log(`nombre: ${nombre}, edad: ${edad}`);
+    console.error("Error: Haga otra vez");
+    console.warn("Advertencia: X yX");
+}
+function arreglos(){
+    let frutas = ['manzana', 'pera', 'banana'];
+    for (let f of frutas ){
+        console.log(f)
+    }
+    if (frutas[1]=== 'pera'){
+        console.log("La segunda fruta es una pera");
+    
+        }
+        else{
+            console.log('No es una pera')
+        }
+    
+}
